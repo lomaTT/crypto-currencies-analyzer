d3568466f6b912a88e27b44927d0a39a9b7b3227
lomaTT <alexeyttdeveloper@gmail.com>
2023-05-29 17:40:07 +0200

    initial commit

---
+++ CryptoAnalyzerProject/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CryptoAnalyzerProject;
+
+XMLimport str = new XMLimport();
+str.exportFromCSV("D:/Projects/crypto-currencies-analyzer/archive");
