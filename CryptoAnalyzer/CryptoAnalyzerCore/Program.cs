78e6de40fd4f038b53b69bc920f658eb0c915b30
IntCat13 <85766091+IntCat13@users.noreply.github.com>
2023-06-04 20:43:45 +0200

    A basic project structure was created
    A new C# project was created consisting of two elements: the client with the user interface and the application core, where all the logic will be located.

---
+++ CryptoAnalyzer/CryptoAnalyzerCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoAnalyzerCore;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        XMLimport str = new XMLimport();
+        str.exportFromCSV("D:/C#-projs/crypto-currencies-analyzer/archive");
+    }
+}
--- CryptoAnalyzerProject/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using CryptoAnalyzerProject;
-
-XMLimport str = new XMLimport();
-str.exportFromCSV("D:/Projects/crypto-currencies-analyzer/archive");
